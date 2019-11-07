8d58e749f809779e156db72160754972af4920c0
Sahil Kapur <sahilkapur.a@gmail.com>
2019-11-06 21:13:41 -0500

    added wheel for python build

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ echo "Installing python dependencies...";
 cd ../python;
 python3 -m venv ./env
 source env/bin/activate;
+pip install wheel
 pip install -r requirements.txt;
 deactivate;
 echo "Python dependencies successfully installed.";
